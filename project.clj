d6aa7ea12e50927bee5cdbc6d2f7053fa7a883a7
Heehong Moon <bbirec@gmail.com>
2013-01-10 09:15:09 +0900

    Added lein project file.

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject bandalore "0.0.3"
+  :description "A Clojure client library for Amazon's Simple Queue Service (SQS). Forked from https://github.com/cemerick/bandalore."
+  :url "https://github.com/bbirec/bandalore"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.amazonaws/aws-java-sdk "1.3.26"]]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"])
