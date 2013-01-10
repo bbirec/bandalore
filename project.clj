2f2ee2413f962812f4a5d2da882badf90c9cf431
Heehong Moon <bbirec@gmail.com>
2013-01-10 10:49:04 +0900

    Changed groupId for clojars.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bandalore "0.0.3"
+(defproject org.clojars.bbirec/bandalore "0.0.3-SNAPSHOT"
   :description "A Clojure client library for Amazon's Simple Queue Service (SQS). Forked from https://github.com/cemerick/bandalore."
   :url "https://github.com/bbirec/bandalore"
   :license {:name "Eclipse Public License"
